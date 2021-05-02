d4f33e4528f0e94d73fc63ad4dd49b420899306c
SWATHI642 <78651885+SWATHI642@users.noreply.github.com>
2021-05-02 20:21:20 +0530

    Selenium testcases app

---
+++ src/main/java/com/mycompany/com/addressbook_selenium_ide/App.java
@@ -0,0 +1,38 @@
+package com.mycompany.com.addressbook_selenium_ide;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+
+
+public class App 
+{   
+	@Test
+    public static void main( String[] args )
+    {
+		System.setProperty("webdriver.chrome.driver","home/ubuntu/selenium/chromedriver");
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.addArguments("---headless");
+		 WebDriver driver = new ChromeDriver(chromeOptions);
+
+		 System.out.println("Hi, Welcome to selenuim with jenkins");
+
+
+		 driver.get("http://52.66.235.24:8998//addressbook");
+
+		 driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+		 driver.findElement(By.className("v-button")).click();
+		 driver.findElement(By.id("gwt-uid-5")).sendKeys("SwathiJenkins");
+		 driver.findElement(By.id("gwt-uid-7")).sendKeys("ManjunathJenkins");
+		 driver.findElement(By.id("gwt-uid-9")).sendKeys("999996667");
+		 driver.findElement(By.id("gwt-uid-11")).sendKeys("Devops@edu.com");
+		 driver.findElement(By.id("gwt-uid-13")).sendKeys("02/21/1998");
+		 driver.findElement(By.className("v-button-primary")).click();
+		 //Thread.sleep(5000);
+		 //driver.quit();
+
+    }
+}
